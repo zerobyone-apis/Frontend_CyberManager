5dfe73cc3afd451f1165d71074f33e3550c30315
Maximiliano Olivero <37275050+Max-Oliver@users.noreply.github.com>
2022-12-02 12:29:51 -0300

    Update IntegrationBackend.ts

--- src/utils/IntegrationBackend.ts
+++ src/utils/IntegrationBackend.ts
@@ -1,8 +1,13 @@
 export default class IntegrationBackend {
-  private baseUrl = 'https://cyber-manager-back-v2.herokuapp.com'; //-> Postgres = Latest v4
-  //private baseUrl = 'https://cyber-manager-backend-pg.herokuapp.com';
+    private baseUrl = 'https://backendcybermanager-production.up.railway.app'; //-> Postgres = Latest v5
+  // private baseUrl = 'https://cyber-manager-back-v2.herokuapp.com'; //-> Postgres = Latest v4
+  // private baseUrl = 'https://cyber-manager-backend-pg.herokuapp.com';
   // private baseUrl = 'https://cyber-manager-backend-pg.herokuapp.com/'; //-> Postgres = Latest v3
-  //private baseUrl = 'http://localhost:3000';
+  // private baseUrl = 'http://localhost:3000';
+  
+  // New URL DEPLOYED: www.railway.com/
+  // https://backendcybermanager-production.up.railway.app/
+  
   private axios = require('axios');
 
   async send(method: string, data?: any, route?: string) {
